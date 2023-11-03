e9ec9054ce9fc49f1a802b642f463af082a0079a
SaraEOlivera <saraeolivera@gmail.com>
2023-11-03 10:06:54 -0300

    Primeros archivos HTML y de estilo

---
+++ app/ejercicio1.js
@@ -0,0 +1,18 @@
+// Ejercicio 1
+// Tengo desarrollada la siguiente función en javascript
+
+// function suma (a,b,callback){
+// let c = a +b;
+// callback( );
+// }
+
+// ¿Que es el tercer parámetro recibido?
+//El tercer parametro que recibe suma() es una funcion callback
+
+// ¿En que casos es obligatorio desarrollar este tipo de funciones?
+//Las funciones callback son utiles para asegurarse de que un determinado código no se ejecuta hasta que otro codigo haya terminado de ejecutarse.
+
+// Realizar un llamado a la función de ejemplo
+//suma(2,3,callback)
+
+
